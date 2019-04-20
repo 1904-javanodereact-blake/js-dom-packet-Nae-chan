46c9c99a83abc0644384ed40ea528e1184848f7b
nae-chan <danaemorg@gmail.com>
2019-04-20 17:57:40 -0400

    Upload answers 1-5

--- index.js
+++ index.js
@@ -5,26 +5,59 @@
 // Define function getUSA()
 // Find the html element that contains "USA".
 // Print that element's contents.
-document.getElementById
+function getUSA () {
+    const findUsa = document.getElementsByTagName("span")
+    for (let i = 0; i<findUsa.length; i++){
+        if ( findUsa[i].innerText === "USA") {
+        console.log (findUsa[i].innerText);
+            }
+    }
+}
 // 2. Sales
 // Define function getPeopleInSales()
 // Print the names of all the people in the sales department.
-
+function getPeopleInSales (){
+    const empName = document.getElementsByClassName("empName")
+    for (let i = 0; i<empName.length; i++) {
+        console.log (empName[i])
+    }return;
+};
+//}
 // 3. Click Here
 // Define function getAnchorChildren()
 // Find all anchor elements with a <span> child.
 // Print the contents of <span>
+function getAnchorChildren (){
+    const getAnchorSpans = document.getElementsByTagName("span");
+    for (let i = 0; i< getAnchorSpans.length; i++) {
+        if (getAnchorSpans[i].parentElement.localName === "a"){
+            console.log (getAnchorSpans[i].innerText)
+        }
+    }
+}
+
 
 // 4. Hobbies
 // Define function getHobbies()
 // Find all checked options in the 'skills' select element.
 // Print the value and the contents.
+function getHobbies(){
+    const element = document.getElementsByName("skills")["0"].value
+    return console.log(element);
+}
 
 // 5. Custom Attribute
 // Define function getCustomAttribute()
 // Find all elements with "data-customAttr" attribute
 // Print the value of the attribute.
 // Print the element that has the attribute. 
+function getCustomAttribute(){
+    const attribute = document.querySelectorAll('[data-customAttr]');
+    for (let i = 0; i < attribute.length; i++) {
+        console.log(attribute[i].nodeName)
+        console.log(attribute[i].innerText)
+    }
+}
 
 // 6. Sum Event
 // NOTE: Write unobtrusive Javascript
@@ -37,6 +70,15 @@ document.getElementById
 // Add <input> element values.
 // Put the sum in the <span> element.
 // If values cannot be added, put "Cannot add" in the <span> element
+<input type="number" oninput="sum(num1, num2)"></input>
+
+function sum (){
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    totalSum = num1 + num2;
+    document.getElementById("sum").value = totalSum;
+}
+
 // 7. Skills Event
 // NOTE: Write unobtrusive Javascript
 // When user selects a skill, create an alert with a message similar to:
